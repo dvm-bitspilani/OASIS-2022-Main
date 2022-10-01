2f9e1b53423f3e3c02d1c16a8d6da5f610fd345e
Prateek Kashyap <kashyapprateek13@gmail.com>
2022-10-01 09:34:46 +0530

    feat: scroll to top

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,8 +2,10 @@ import React from "react";
 import Lander from "../components/Lander";
 import Registration from "./Registration";
 import { useState } from "react";
+import Contact from "../components/Contact";
 
 export default function Home() {
+  window.scrollTo(0, 0);
   const [regState, setRegState] = useState({ open: false });
 
   const changeRegState = () => {
@@ -18,6 +20,7 @@ export default function Home() {
         changeRegState={changeRegState}
       />
       <Lander changeRegState={changeRegState} />
+      <Contact />
     </main>
   );
 }
